a74e914f987b5f1402d181f92e5b6428467e9a0a
PaulKrafczuk <krafczukpaul@gmail.com>
2021-03-15 23:08:33 -0300

    accordion done

--- js/main.js
+++ js/main.js
@@ -1,37 +1,41 @@
 $(document).ready(function () {
-    //slider
-    $('.slider').bxSlider({
-        mode: 'fade',
-        captions: true,
-        slideWidth: 1200
-    });
 
-    //posts
-    var posts = [
-        {
-            title: 'prueba de titulo 1',
-            date: 'Publicado el dia ' + moment().date() + ' de ' + moment().format('MMM') + ' del ' + moment().format('YYYY'),
-            content: 'da sunt in nihil vel dolorem, dolores repellat, maiores soluta repellendus adipiscicupiditate fugiat.Place'
-        },
-        {
-            title: 'prueba de titulo 2',
-            date: 'Publicado el dia ' + moment().date() + ' de ' + moment().format('MMM') + ' del ' + moment().format('YYYY'),
-            content: 'da sunt in nihil vel dolorem, dolores repellat, maiores soluta repellendus adipiscicupiditate fugiat.Place'
-        },
-        {
-            title: 'prueba de titulo 3',
-            date: 'Publicado el dia ' + moment().date() + ' de ' + moment().format('MMM') + ' del ' + moment().format('YYYY'),
-            content: 'da sunt in nihil vel dolorem, dolores repellat, maiores soluta repellendus adipiscicupiditate fugiat.Place'
-        },
-        {
-            title: 'prueba de titulo 4',
-            date: 'Publicado el dia ' + moment().date() + ' de ' + moment().format('MMM') + ' del ' + moment().format('YYYY'),
-            content: 'da sunt in nihil vel dolorem, dolores repellat, maiores soluta repellendus adipiscicupiditate fugiat.Place'
-        }
-    ];
 
-    posts.forEach((item) => {
-        var post = `
+    if (window.location.href.indexOf('index') > -1) {
+
+        //slider
+        $('.slider').bxSlider({
+            mode: 'fade',
+            captions: true,
+            slideWidth: 1200
+        });
+
+        //posts
+        var posts = [
+            {
+                title: 'prueba de titulo 1',
+                date: 'Publicado el dia ' + moment().date() + ' de ' + moment().format('MMM') + ' del ' + moment().format('YYYY'),
+                content: 'da sunt in nihil vel dolorem, dolores repellat, maiores soluta repellendus adipiscicupiditate fugiat.Place'
+            },
+            {
+                title: 'prueba de titulo 2',
+                date: 'Publicado el dia ' + moment().date() + ' de ' + moment().format('MMM') + ' del ' + moment().format('YYYY'),
+                content: 'da sunt in nihil vel dolorem, dolores repellat, maiores soluta repellendus adipiscicupiditate fugiat.Place'
+            },
+            {
+                title: 'prueba de titulo 3',
+                date: 'Publicado el dia ' + moment().date() + ' de ' + moment().format('MMM') + ' del ' + moment().format('YYYY'),
+                content: 'da sunt in nihil vel dolorem, dolores repellat, maiores soluta repellendus adipiscicupiditate fugiat.Place'
+            },
+            {
+                title: 'prueba de titulo 4',
+                date: 'Publicado el dia ' + moment().date() + ' de ' + moment().format('MMM') + ' del ' + moment().format('YYYY'),
+                content: 'da sunt in nihil vel dolorem, dolores repellat, maiores soluta repellendus adipiscicupiditate fugiat.Place'
+            }
+        ];
+
+        posts.forEach((item) => {
+            var post = `
                 <article class="post">
                     <h2>${item.title}</h2>
                     <span class="date">${item.date}</span>
@@ -39,8 +43,9 @@ $(document).ready(function () {
                     <a href="#" class="button-more">Leer mas</a>
                 </article>`;
 
-        $("#posts").append(post);
-    });
+            $("#posts").append(post);
+        });
+    };
 
     //scroll to top
     $('.subir').click((e) => {
@@ -51,4 +56,32 @@ $(document).ready(function () {
         return false;
     });
 
+    //login falso
+
+    $('#login form').submit(function () {
+        let form_name = $('#form-name').val();
+        localStorage.setItem('form_name', form_name);
+    });
+
+    let form_name = localStorage.getItem('form_name');
+
+    if (form_name != null && form_name != undefined) {
+        var about_parrafo = $('#about p');
+
+        about_parrafo.html('<br><strong>Bienvenido ' + form_name + '</strong></br>');
+        about_parrafo.append("<a href='#' id='logout'>Cerrar sesion</a>");
+        $('#login').hide();
+
+        $('#logout').click(() => {
+            localStorage.clear();
+            location.reload();
+        });
+    };
+
+    if (window.location.href.indexOf('about') > -1) {
+        console.log('estoy en about')
+        $('#acordeon').accordion();
+    };
+
 });
+
